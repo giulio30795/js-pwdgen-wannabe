51f3ce89b6fbe3b410825b0860bfcc4cc2aaf118
giulio30795 <giulioincarbone95@gmail.com>
2021-10-20 14:30:27 +0200

    const

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+//      Password generator:
+// - chiedi all’utente il suo nome,
+// - poi chiedi il suo cognome,
+// - poi chiedi il suo colore preferito
+// - infine scrivi sulla pagina i dati ricevuti nel formato: nomecognomecolorepreferito21
+
+const Name = prompt('Qual è il tuo nome?');
+const Surname = prompt('Qual è il tuo cognome?');
+const Color = prompt('Qual è il tuo colore preferito?');
+
+document.getElementById('password').innerHTML = Name + Surname + Color + '21'
